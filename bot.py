b1e9a693f90f997794163d1c8ea04ea5d8086ce7
botdeneme-ui <botdeneme@outlook.com>
2026-07-03 15:35:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import os
 import requests
 from bs4 import BeautifulSoup
 
@@ -7,15 +8,61 @@ headers = {
     "User-Agent": "Mozilla/5.0"
 }
 
-response = requests.get(URL, headers=headers)
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
 
-print("Durum kodu:", response.status_code)
+response = requests.get(URL, headers=headers)
+response.raise_for_status()
 
 soup = BeautifulSoup(response.text, "html.parser")
 
-basliklar = soup.select(".structItem-title a")
+konular = soup.select(".structItem--thread")
 
-print("Bulunan başlık sayısı:", len(basliklar))
+if len(konular) < 5:
+    raise Exception("Yeterli konu bulunamadı.")
 
-for i, konu in enumerate(basliklar[:10], start=1):
-    print(f"{i}. {konu.get_text(strip=True)}")
+# İlk 4 sabit konuyu geç
+konu = konular[4]
+
+baslik = konu.select_one(".structItem-title a").get_text(strip=True)
+link = "https://forum.donanimarsivi.com" + konu.select_one(".structItem-title a")["href"]
+
+print("Takip edilen konu:", baslik)
+
+DOSYA = "last_topic.txt"
+
+if os.path.exists(DOSYA):
+    with open(DOSYA, "r", encoding="utf-8") as f:
+        eski = f.read().strip()
+else:
+    eski = ""
+
+if eski == "":
+    print("İlk çalıştırma.")
+    with open(DOSYA, "w", encoding="utf-8") as f:
+        f.write(baslik)
+
+elif eski != baslik:
+
+    mesaj = f"""🔥 Yeni Konu Açıldı!
+
+{baslik}
+
+{link}
+"""
+
+    requests.get(
+        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+        params={
+            "chat_id": CHAT_ID,
+            "text": mesaj
+        }
+    )
+
+    print("Telegram bildirimi gönderildi.")
+
+    with open(DOSYA, "w", encoding="utf-8") as f:
+        f.write(baslik)
+
+else:
+    print("Yeni konu yok.")
